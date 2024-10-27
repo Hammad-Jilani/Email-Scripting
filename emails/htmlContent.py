b88229c1b3463b8f74989b4071f9a5a59f207245
Hammad-Jilani <hjilani15@gmail.com>
2024-10-27 20:35:28 +0500

    EMail script ready

---
+++ emails/htmlContent.py
@@ -0,0 +1,105 @@
+def HtmlContent():
+  return f""" 
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body {{
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }}
+        .container {{
+            width: 100%;
+            max-width: 600px;
+            margin: 20px auto;
+            background-color: #ffffff;
+            border-radius: 8px;
+            overflow: hidden;
+            box-shadow: 0 2px 10px rgba(0,0,0,0.1);
+        }}
+        .header {{
+            background-color: #007BFF;
+            color: #ffffff;
+            padding: 20px;
+            text-align: center;
+        }}
+        .content {{
+            padding: 20px;
+        }}
+        h1 {{
+            font-size: 24px;
+            margin: 0 0 10px;
+        }}
+        h2 {{
+            font-size: 20px;
+            margin: 20px 0 10px;
+        }}
+        p {{
+            line-height: 1.6;
+        }}
+        .event-details, .expectations {{
+            margin: 20px 0;
+            padding: 10px;
+            border-left: 5px solid #007BFF;
+            background-color: #f9f9f9;
+        }}
+        .footer {{
+            padding: 20px;
+            text-align: center;
+            background-color: #f4f4f4;
+            border-top: 1px solid #ddd;
+        }}
+        a {{
+            color: #007BFF;
+            text-decoration: none;
+        }}
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Get Ready to Revolutionize!</h1>
+        </div>
+        <div class="content">
+            <p>Dear All,</p>
+            <p>The wait is over! TGD'24 is coming soon!</p>
+            <p>Join us for an unforgettable experience of inspiration, innovation, and connection!</p>
+
+            <div class="event-details">
+                <h2>Event Details:</h2>
+                <p><strong>Event Name:</strong> TGD'24</p>
+                <p><strong>Date:</strong> 20th & 21st November</p>
+                <p><strong>Location:</strong> FAST-NUCES (Main Campus)</p>
+            </div>
+
+            <div class="expectations">
+                <h2>What to Expect:</h2>
+                <ul>
+                    <li>Inspiring keynotes and panel discussions</li>
+                    <li>Best Delegation Awards</li>
+                    <li>Unparalleled networking opportunities</li>
+                </ul>
+            </div>
+
+            <h2>Stay Updated:</h2>
+            <p>Follow us on social media for updates, sneak peeks, and behind-the-scenes insights:</p>
+            <p>Instagram: <a href="https://www.instagram.com/tlc_nu?igsh=YzljYTk1ODg3Zg==">Our Instagram Page</a></p>
+            <p>Facebook: <a href="https://www.facebook.com/TLCFAST?mibextid=ZbWKwL">Our Facebook Page</a></p>
+
+            <h2>Get in Touch:</h2>
+            <p>Email Us: <a href="mailto:tlc.khi@nu.edu.pk">tlc.khi@nu.edu.pk</a></p>
+        </div>
+        <div class="footer">
+            <p>Looking forward to seeing you at TGD'24!</p>
+            <p>Best Regards,<br>The Literary Society,<br>FAST-NUCES.</p>
+        </div>
+    </div>
+</body>
+</html>
+
+
+"""
---
+++ emails/main.py
@@ -0,0 +1,52 @@
+import openpyxl
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from concurrent.futures import ThreadPoolExecutor
+from htmlContent import HtmlContent
+
+path = "C:\\Users\\CoreCom\\Downloads\\final.xlsx"
+
+work = openpyxl.load_workbook(path)
+sheet = work.active
+
+gmail_user = ''
+gmail_password = ''
+
+def send_email(i):
+  email_receiver = sheet.cell(row=i,column=2).value
+  
+  if email_receiver is not None:
+    
+    msg = MIMEMultipart('alternative')
+    msg['From'] = gmail_user
+    msg['To'] = email_receiver
+    msg['Subject'] = "TGD'24: TLC's Flagship Event - Don't Miss Out!"
+    html_email = HtmlContent()
+    msg.attach(MIMEText(html_email,'html'))
+
+    pdf_filename = r'C:\Users\CoreCom\Downloads\tlc.pdf'
+    with open(pdf_filename, 'rb') as pdf_file:
+      pdf_attachment = MIMEApplication(pdf_file.read(), _subtype='pdf')
+      pdf_attachment.add_header('Content-Disposition', f'attachment; filename="TLC.pdf"')
+      msg.attach(pdf_attachment)
+    
+    
+    try:
+      server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+      server.login(gmail_user, gmail_password)
+      server.sendmail(gmail_user, email_receiver, msg.as_string())
+      server.close()
+      sheet.cell(row=i,column=8).value="Send"
+      print(f"Email send to {email_receiver}")
+      work.save(path)
+
+    except Exception as e:
+      sheet.cell(row=i,column=8).value='Unsend'
+      print(e)
+      print(f"Did not send to {email_receiver}")
+      work.save(path)
+    
+with ThreadPoolExecutor(max_workers=1) as executor:
+    futures = [executor.submit(send_email, i) for i in range(1, sheet.max_row + 1)]
