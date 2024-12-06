e445fb7443566bfc91efb6a0104a3ded8d9afbcc
Hammad-Jilani <hjilani15@gmail.com>
2024-12-06 17:37:12 +0500

    tlc certificates

--- emails/certificate.py
+++ emails/certificate.py
@@ -1,52 +1,63 @@
 def certificates(name):
-  return f""" 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Certificates</title>
-</head>
-<style>
-  *{{
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }}
-  .container{{
-    position: relative;
-    height: auto;
-  }}
-  .container img{{
-    width: 100%;
-    height: 100%;
-    z-index: 1;
-    /* object-fit: cover; */
-  }}
-
-  p{{
-    width: 82%;
-    position: absolute;
-    padding: 10px;
-    font-family: Calibri, sans-serif;
-    font-size: 6vw;
-  }}
-  @media screen and (min-width:400px){{
-    p{{
-      top: 46%;
-      left: 22%;
-    }}
-  }}
-
-</style>
-<body>
-  <div class="container">
-    <img src="/emails/Images/image.png" alt="Letter">
-    <p>
-      {name}
-    </p>
-  </div>
-</body>
-</html>
-"""
+  return f"""
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Email Automation</title>
+        </head>
+        <style>
+          *{{
+            box-sizing: border-box;
+            margin: 0;
+            padding: 0;
+          }}
+          .container{{
+            position: relative;
+          }}
+          .container img{{
+            width: 100%;
+            z-index: 1;
+          }}
+          p{{
+            width: 78%;
+            position: absolute;
+            left: 26%;
+            top: 47%;
+            font-size: 30px;
+            padding: 10px;
+            font-family: Calibri, sans-serif;
+            margin-top: 20px; /* Reduced space above the text */
+          }}
+          @media screen and (max-width:600px) {{
+            p{{
+              width: 80%;
+              font-size: 2vw; /* Adjusted for smaller screens */
+              white-space: wrap;
+            }}
+            .container{{
+              white-space: wrap;
+            }}
+          }}
+          @media screen and (min-width:600px) {{
+            p{{
+              width: 80%;
+              font-size: 2.3vw;
+              white-space: wrap;
+              left: 26%;
+              top: 47%;
+              font-size: 5vw;
+            }}
+          }}
+        </style>
+        <body>
+          <div class="container">
+            <img src="C:\\Users\\CoreCom\\Downloads\\certificate.png" alt="Certificates">
+            <p>
+              {name}
+            </p>
+          </div>
+        </body>
+        </html>
+        """
--- emails/tempCodeRunnerFile.py
+++ emails/tempCodeRunnerFile.py
@@ -1,5 +1,2 @@
-eCom\Downloads\The Grand Debate '24.pdf"
-    # with open(pdf_filename, 'rb') as pdf_file:
-    #   pdf_attachment = MIMEApplication(pdf_file.read(), _subtype='pdf')
-    #   pdf_attachment.add_header('Content-Disposition', f'attachment; filename="The Grand Debate 24"')
-    #   msg.attach(pdf_attachment)
+      top: 47%;
+              font-size: 5vw;
